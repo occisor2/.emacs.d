e5558cecfbf8adf09fe4771d2823377b3c3d49ca
Russell Smith <russell.smith7502@gmail.com>
2022-05-28 18:02:21 -0500

    Worked on replacement for file shortcut macro

--- local/mylib.el
+++ local/mylib.el
@@ -27,6 +27,35 @@ cursor at its beginning, according to the current mode."
       nil
     t))
 
+(defmacro my-wrap-command (&rest body)
+  "Wrap forms in an interactive lambda."
+  `(lambda ()
+     (interactive)
+     ,@body))
+
+;; (defmacro my-create-fileshortcuts (&rest body)
+;;   ;; (cl-loop
+;;   ;;  repeat (/ (length lst) 2)
+;;   ;;  for a = lst then (cddr a)
+;;   ;;  and b = (cdr lst) then (cddr b)
+;;   ;;  collect (list (car a) (car b)))
+;;   (push 'my-leader-def
+;;         (cl-loop
+;;          for i to (length body)
+;;          for x in body
+;;          if (cl-evenp i)
+;;          collect `(,@(concat "f " x)) into shortcuts
+;;          else
+;;          collect `(quote
+;;                    (,(my-wrap-command
+;;                      (find-file (expand-file-name x)))
+;;                     :wk ,(file-name-nondirectory x)))
+;;          into shortcuts
+;;          finally (return shortcuts))))
+
+;; (my-create-fileshortcuts
+;;  "s" "~/.bashrc")
+
 (defmacro my-filesc-def (&rest body)
   "A wrapper over my-leader-def that elimates the need for boiler plate code for
 defining lambdas around find-file commands.
