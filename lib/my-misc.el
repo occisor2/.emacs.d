fa5b1e8913ecec93003cbd04177deb8e50c34207
Russell Smith <res565@CAVS.msstate.edu>
2023-06-07 18:09:03 -0500

    Add a macro for defining keyboard shorcuts

--- lib/my-misc.el
+++ lib/my-misc.el
@@ -1,5 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
+(use-package dash)
+
 (defmacro with-system (type &rest body)
   "Evaluate BODY if `system-type' equals TYPE."
   (declare (indent defun))
@@ -9,5 +11,30 @@
 (defun font-exists-p (font)
   "Check if font is available"
   (if (null (x-list-fonts font)) nil t))
+
+(defmacro def-shortcut (prefix &rest body)
+  "A definer for file shorcuts.
+
+Shortcut keybinds are appending to `prefix' and are given a which-key
+hint that is the file name.
+
+The prefix should be a valid emacs prefix.
+Body syntax:
+(def-shortcut prefix
+  ([Keybind string] [File name])
+  ...)"
+  (declare (indent defun))
+  `(general-def
+     :prefix ,prefix
+     ,@(-flatten-n
+        1
+        (cl-loop
+         for x in body collect
+         (list
+          (car x)
+          `'((lambda ()
+               (interactive)
+               (find-file ,(cadr x)))
+             :which-key ,(file-name-nondirectory (cadr x))))))))
 
 (provide 'my-misc)
