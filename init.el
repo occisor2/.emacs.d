bf2c9682aa667c033c941e5608f831a8c80c2c17
Russell Smith <russell.smith7502@gmail.com>
2024-11-25 09:25:13 -0600

    Properly setup treesitter

--- init.el
+++ init.el
@@ -412,12 +412,28 @@
            (eq system-type 'gnu/linux))
   (use-package tree-sitter
     :defer t
-    :hook
-    ((c++-mode . (lambda ()
-                   (tree-sitter-mode 1)
-                   (tree-sitter-hl-mode 1))))
     :config
-    (use-package tree-sitter-langs)))
+    (setq treesit-language-source-alist
+          '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+            (cmake "https://github.com/uyha/tree-sitter-cmake")
+            (css "https://github.com/tree-sitter/tree-sitter-css")
+            (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+            (go "https://github.com/tree-sitter/tree-sitter-go")
+            (html "https://github.com/tree-sitter/tree-sitter-html")
+            (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+            (json "https://github.com/tree-sitter/tree-sitter-json")
+            (make "https://github.com/alemuller/tree-sitter-make")
+            (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+            (python "https://github.com/tree-sitter/tree-sitter-python")
+            (toml "https://github.com/tree-sitter/tree-sitter-toml")
+            (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+            (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+            (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+            (c "https://github.com/tree-sitter/tree-sitter-c")
+            (cpp "https://github.com/tree-sitter/tree-sitter-cpp")))
+
+    (add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+    (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))))
 
 ;; Avy
 (use-package avy
@@ -693,11 +709,14 @@
 
   (setq-default c-auto-newline t))
 
-(use-package nasm-mode)
+(use-package c-ts-mode
+  :if (treesit-available-p)
+  :config
+  (setq c-ts-mode-indent-style 'Linux
+        c-ts-mode-indent-offset 4))
 
-(use-package disaster
-  :init
-  (setq disaster-assembly-mode 'nasm-mode))
+;; Assembly
+(use-package nasm-mode)
 
 ;; Cmake
 (use-package cmake-mode
